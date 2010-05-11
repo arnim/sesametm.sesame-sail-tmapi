643fc389c85e69bc43299ab44fabace9a37fb3df
arnim.bleier <arnim.bleier@localhost>
2010-05-11 13:31:46 +0200

    now  addresses of TopicMap instances known can be get as contextes of rdf graphs

--- src/main/java/de/topicmapslab/sesametm/tmapi2tm/TmapiSailConnection.java
+++ src/main/java/de/topicmapslab/sesametm/tmapi2tm/TmapiSailConnection.java
@@ -4,6 +4,7 @@
 package de.topicmapslab.sesametm.tmapi2tm;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import info.aduna.iteration.CloseableIteration;
 import info.aduna.iteration.CloseableIterationBase;
@@ -22,17 +23,17 @@ import org.openrdf.query.algebra.TupleExpr;
 import org.openrdf.sail.SailException;
 import org.openrdf.sail.helpers.SailBase;
 import org.openrdf.sail.helpers.SailConnectionBase;
+import org.tmapi.core.Locator;
 import org.tmapi.core.TopicMap;
 import org.tmapi.core.TopicMapExistsException;
 import org.tmapi.core.TopicMapSystem;
 
 /**
  * @author Arnim Bleier
- *
+ * 
  */
 public class TmapiSailConnection extends SailConnectionBase {
-	
-	
+
 	private TopicMapSystem tmSystem;
 
 	/**
@@ -43,8 +44,13 @@ public class TmapiSailConnection extends SailConnectionBase {
 		tmSystem = ((TmapiStore) sailBase).getTmSystem();
 	}
 
-	/* (non-Javadoc)
-	 * @see org.openrdf.sail.helpers.SailConnectionBase#addStatementInternal(org.openrdf.model.Resource, org.openrdf.model.URI, org.openrdf.model.Value, org.openrdf.model.Resource[])
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openrdf.sail.helpers.SailConnectionBase#addStatementInternal(org.
+	 * openrdf.model.Resource, org.openrdf.model.URI, org.openrdf.model.Value,
+	 * org.openrdf.model.Resource[])
 	 */
 	@Override
 	protected void addStatementInternal(Resource arg0, URI arg1, Value arg2,
@@ -58,8 +64,12 @@ public class TmapiSailConnection extends SailConnectionBase {
 		}
 	}
 
-	/* (non-Javadoc)
-	 * @see org.openrdf.sail.helpers.SailConnectionBase#clearInternal(org.openrdf.model.Resource[])
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openrdf.sail.helpers.SailConnectionBase#clearInternal(org.openrdf
+	 * .model.Resource[])
 	 */
 	@Override
 	protected void clearInternal(Resource... arg0) throws SailException {
@@ -67,8 +77,11 @@ public class TmapiSailConnection extends SailConnectionBase {
 
 	}
 
-	/* (non-Javadoc)
-	 * @see org.openrdf.sail.helpers.SailConnectionBase#clearNamespacesInternal()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openrdf.sail.helpers.SailConnectionBase#clearNamespacesInternal()
 	 */
 	@Override
 	protected void clearNamespacesInternal() throws SailException {
@@ -76,7 +89,9 @@ public class TmapiSailConnection extends SailConnectionBase {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.openrdf.sail.helpers.SailConnectionBase#closeInternal()
 	 */
 	@Override
@@ -85,7 +100,9 @@ public class TmapiSailConnection extends SailConnectionBase {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.openrdf.sail.helpers.SailConnectionBase#commitInternal()
 	 */
 	@Override
@@ -94,8 +111,13 @@ public class TmapiSailConnection extends SailConnectionBase {
 
 	}
 
-	/* (non-Javadoc)
-	 * @see org.openrdf.sail.helpers.SailConnectionBase#evaluateInternal(org.openrdf.query.algebra.TupleExpr, org.openrdf.query.Dataset, org.openrdf.query.BindingSet, boolean)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openrdf.sail.helpers.SailConnectionBase#evaluateInternal(org.openrdf
+	 * .query.algebra.TupleExpr, org.openrdf.query.Dataset,
+	 * org.openrdf.query.BindingSet, boolean)
 	 */
 	@Override
 	protected CloseableIteration<? extends BindingSet, QueryEvaluationException> evaluateInternal(
@@ -105,41 +127,30 @@ public class TmapiSailConnection extends SailConnectionBase {
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.openrdf.sail.helpers.SailConnectionBase#getContextIDsInternal()
 	 */
 	@Override
 	protected CloseableIteration<? extends Resource, SailException> getContextIDsInternal()
 			throws SailException {
-		System.out.println("wichtige in CloseableIteration 3");
-		CloseableIteration<? extends Resource, SailException> result = new CloseableIterationBase<Resource, SailException>() {
-
-			public boolean hasNext() throws SailException {
-				// TODO Auto-generated method stub
-				return false;
-			}
-
-			public Resource next() throws SailException {
-				// TODO Auto-generated method stub
-				return null;
-			}
-
-			public void remove() throws SailException {
-				// TODO Auto-generated method stub
-				
-			}};
-			System.out.println("  33");
-			
-			ArrayList<Resource> contextIDs = new ArrayList<Resource>(32);
-			contextIDs.add(new URIImpl("http://www.openrdf.org/doc/sesame2/2.3.1/apidocs/"));
-
-			CloseableIteratorIteration<Resource, SailException> result2 = new CloseableIteratorIteration<Resource, SailException>(contextIDs.iterator());
-			
+		ArrayList<Resource> contextIDs = new ArrayList<Resource>();
+		Iterator<Locator> allBaseIris = tmSystem.getLocators().iterator();
+		while (allBaseIris.hasNext()) {
+			contextIDs.add(new URIImpl(allBaseIris.next().toExternalForm()));
+		}
+		CloseableIteratorIteration<Resource, SailException> result2 = new CloseableIteratorIteration<Resource, SailException>(
+				contextIDs.iterator());
 		return result2;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.openrdf.sail.helpers.SailConnectionBase#getNamespaceInternal(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openrdf.sail.helpers.SailConnectionBase#getNamespaceInternal(java
+	 * .lang.String)
 	 */
 	@Override
 	protected String getNamespaceInternal(String arg0) throws SailException {
@@ -147,7 +158,9 @@ public class TmapiSailConnection extends SailConnectionBase {
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.openrdf.sail.helpers.SailConnectionBase#getNamespacesInternal()
 	 */
 	@Override
@@ -157,8 +170,13 @@ public class TmapiSailConnection extends SailConnectionBase {
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.openrdf.sail.helpers.SailConnectionBase#getStatementsInternal(org.openrdf.model.Resource, org.openrdf.model.URI, org.openrdf.model.Value, boolean, org.openrdf.model.Resource[])
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openrdf.sail.helpers.SailConnectionBase#getStatementsInternal(org
+	 * .openrdf.model.Resource, org.openrdf.model.URI, org.openrdf.model.Value,
+	 * boolean, org.openrdf.model.Resource[])
 	 */
 	@Override
 	protected CloseableIteration<? extends Statement, SailException> getStatementsInternal(
@@ -168,8 +186,12 @@ public class TmapiSailConnection extends SailConnectionBase {
 		return null;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.openrdf.sail.helpers.SailConnectionBase#removeNamespaceInternal(java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openrdf.sail.helpers.SailConnectionBase#removeNamespaceInternal(java
+	 * .lang.String)
 	 */
 	@Override
 	protected void removeNamespaceInternal(String arg0) throws SailException {
@@ -177,8 +199,13 @@ public class TmapiSailConnection extends SailConnectionBase {
 
 	}
 
-	/* (non-Javadoc)
-	 * @see org.openrdf.sail.helpers.SailConnectionBase#removeStatementsInternal(org.openrdf.model.Resource, org.openrdf.model.URI, org.openrdf.model.Value, org.openrdf.model.Resource[])
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openrdf.sail.helpers.SailConnectionBase#removeStatementsInternal(
+	 * org.openrdf.model.Resource, org.openrdf.model.URI,
+	 * org.openrdf.model.Value, org.openrdf.model.Resource[])
 	 */
 	@Override
 	protected void removeStatementsInternal(Resource arg0, URI arg1,
@@ -187,7 +214,9 @@ public class TmapiSailConnection extends SailConnectionBase {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.openrdf.sail.helpers.SailConnectionBase#rollbackInternal()
 	 */
 	@Override
@@ -196,8 +225,12 @@ public class TmapiSailConnection extends SailConnectionBase {
 
 	}
 
-	/* (non-Javadoc)
-	 * @see org.openrdf.sail.helpers.SailConnectionBase#setNamespaceInternal(java.lang.String, java.lang.String)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openrdf.sail.helpers.SailConnectionBase#setNamespaceInternal(java
+	 * .lang.String, java.lang.String)
 	 */
 	@Override
 	protected void setNamespaceInternal(String arg0, String arg1)
@@ -206,8 +239,12 @@ public class TmapiSailConnection extends SailConnectionBase {
 
 	}
 
-	/* (non-Javadoc)
-	 * @see org.openrdf.sail.helpers.SailConnectionBase#sizeInternal(org.openrdf.model.Resource[])
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openrdf.sail.helpers.SailConnectionBase#sizeInternal(org.openrdf.
+	 * model.Resource[])
 	 */
 	@Override
 	protected long sizeInternal(Resource... arg0) throws SailException {
@@ -215,8 +252,11 @@ public class TmapiSailConnection extends SailConnectionBase {
 		return 0;
 	}
 
-	/* (non-Javadoc)
-	 * @see org.openrdf.sail.helpers.SailConnectionBase#startTransactionInternal()
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.openrdf.sail.helpers.SailConnectionBase#startTransactionInternal()
 	 */
 	@Override
 	protected void startTransactionInternal() throws SailException {
