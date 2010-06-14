0af480a1443166089afca271cef2b96006c56a6d
arnim.bleier <arnim.bleier@localhost>
2010-06-14 16:16:27 +0200

    fitst working impl of owl:sameAs

--- src/main/java/de/topicmapslab/sesame/sail/tmapi/utils/MultiLocatorHandler.java
+++ src/main/java/de/topicmapslab/sesame/sail/tmapi/utils/MultiLocatorHandler.java
@@ -10,11 +10,9 @@ import java.util.Set;
 
 import org.openrdf.model.Statement;
 import org.openrdf.model.vocabulary.OWL;
-import org.openrdf.model.vocabulary.RDF;
 import org.tmapi.core.Locator;
 import org.tmapi.core.Topic;
 import org.tmapi.core.TopicMap;
-import org.tmapi.index.TypeInstanceIndex;
 
 
 /**
@@ -39,19 +37,13 @@ public class MultiLocatorHandler {
 		this.statements = other.getStatements();
 		this.statementFactory = other.getStatementFactory();
 		this.other = other;
-		
-		
-		
-		
 
 	}
 	
 
 
 	public void evaluate(){
-		
-		
-		
+
 		Topic sTopic = null, pTopic = null, oTopic = null;	
 		sTopic = other.getTopic(subj, tm);
 		pTopic = other.getTopic(pred, tm);
@@ -62,83 +54,47 @@ public class MultiLocatorHandler {
 
 				// Q has no match in this tm
 			} else {
-//				if (pTopic == null && pred != null && OWL.SAMEAS.toString().equals(pred.toExternalForm()))
-					createSameAsList(sTopic, oTopic, tm);
-//				else if (sTopic == null && pTopic == null && oTopic == null)
-//					createListXXX(tm);
-//				else if (sTopic != null && pTopic == null && oTopic == null)
-//					createListSXX(sTopic, tm);
-//				else if (sTopic != null && pTopic != null && oTopic == null)
-//					createListSPX(sTopic, pTopic, tm);
-//				else if (sTopic == null && pTopic != null && oTopic == null)
-//					createListXPX(pTopic, tm);
-//				else if (sTopic != null && pTopic != null && oTopic != null)
-//					createListSPO(sTopic, pTopic, oTopic, tm);
-//				else if (sTopic == null && pTopic == null && oTopic != null)
-//					createListXXO(oTopic, tm);
-//				else if (sTopic == null && pTopic != null && oTopic != null)
-//					createListXPO(pTopic, oTopic, tm);
-//				else
-//					System.err
-//							.println("You should never read this! TmapiStatementIterator:104 ");
+				if (sTopic == null && oTopic == null)
+					createSameAsListxPx();
+				else if (sTopic != null && oTopic == null )
+					createSameAsListSPX(sTopic);
+				else if (sTopic == null && oTopic != null)
+					createSameAsListXPO(oTopic);
+				else if (sTopic != null && oTopic != null)
+					createTypeSameAsSPO(sTopic, oTopic);
+				else
+					System.err
+							.println("You should never read this! TmapiStatementIterator:10 ");	
 			}
-		
-		
-	}
-	
-	
-	
-	
-	private void createSameAsList(Topic sTopic, Topic oTopic, TopicMap tm){		
-		if (sTopic == null && oTopic == null)
-			createSameAsListxPx(tm);
-//		else if (sTopic != null && oTopic == null )
-//			createSameAsListSPX(sTopic, tm);
-//		else if (sTopic == null && oTopic != null)
-//			createSameAsListXPO(oTopic, tm);
-//		else if (sTopic != null && oTopic != null)
-//			createTypeSameAsSPO(sTopic, oTopic, tm);
-		else
-			System.err
-					.println("You should never read this! TmapiStatementIterator:104 ");	
-	}
-	
-	
-	private void createSameAsListxPx(TopicMap tm){
-	
-		Iterator<Topic> topicsIterator = tm.getTopics().iterator();
-		while (topicsIterator.hasNext()) {
-			
 
-					
-			createSameAsListSPX(topicsIterator.next(), tm);
+	}
+	
+	
+	
+	
+
+	
+	
+	private void createSameAsListxPx(){
+		Iterator<Topic> topicsIterator = tm.getTopics().iterator();
+		while (topicsIterator.hasNext()) {		
+			createSameAsListSPX(topicsIterator.next());
 		}
 	}
 	
-	private void createSameAsListSPX(Topic sTopic, TopicMap tm){
+	private void createSameAsListSPX(Topic sTopic){
 		Iterator<Locator> otherLotatorsiterator = getOtherLocaters(sTopic).iterator(); 
-		
 		while (otherLotatorsiterator.hasNext()) {
-
 			statements.add(statementFactory.create(sTopic, OWL.SAMEAS, otherLotatorsiterator.next()));
 		}
 	}
 	
-	private void createSameAsListXPO(Topic oTopic, TopicMap tm){
-		Iterator<Topic> subjectIterator = tm.getIndex(TypeInstanceIndex.class).getTopics(oTopic).iterator();
-		while (subjectIterator.hasNext()) {
-			statements.add(statementFactory.create(subjectIterator.next(), RDF.TYPE, oTopic));	
-		}
+	private void createSameAsListXPO(Topic oTopic){
+		createSameAsListSPX(oTopic);
 	}
 	
-	private void createTypeSameAsSPO(Topic sTopic, Topic oTopic, TopicMap tm){
-		Topic objectTopic;
-		Iterator<Topic> typesIterator = sTopic.getTypes().iterator();
-		while (typesIterator.hasNext()) {
-			objectTopic =  typesIterator.next();
-			if (objectTopic.equals(oTopic))
-				statements.add(statementFactory.create(sTopic, RDF.TYPE, objectTopic));
-		}
+	private void createTypeSameAsSPO(Topic sTopic, Topic oTopic){
+//		System.out.println(statementFactory.getBestLocator(sTopic) + "mit " + statementFactory.getBestLocator(oTopic));
 	}
 	
 	
