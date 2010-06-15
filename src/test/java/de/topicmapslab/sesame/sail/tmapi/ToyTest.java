8f8c2c3e87aecdca6abfd4fd247181e8cf2a8a1b
arnim.bleier <arnim.bleier@localhost>
2010-06-15 15:29:34 +0200

    New Test based no ToyTM

---
+++ src/test/java/de/topicmapslab/sesame/sail/tmapi/ToyTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright: Copyright 2010 Topic Maps Lab, University of Leipzig. http://www.topicmapslab.de/
+ * License:   Apache License, Version 2.0 http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+package de.topicmapslab.sesame.sail.tmapi;
+
+import java.io.FileInputStream;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.openrdf.repository.Repository;
+import org.openrdf.repository.RepositoryConnection;
+import org.openrdf.repository.sail.SailRepository;
+import org.openrdf.rio.RDFHandler;
+import org.openrdf.rio.n3.N3Writer;
+import org.tmapi.core.TopicMap;
+import org.tmapi.core.TopicMapSystem;
+import org.tmapi.core.TopicMapSystemFactory;
+import org.tmapix.io.XTM20TopicMapReader;
+
+/**
+ * @author Arnim Bleier
+ * 
+ */
+public class ToyTest extends TestCase {
+
+	private static TmapiStore _sail;
+	private static Repository _tmapiRepository;
+	private static RepositoryConnection _con;
+	private static TopicMapSystem _tms;
+
+	final static String baseIRI = "http://www.topicmapslab.de/test/base/";
+
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		_tms = TopicMapSystemFactory.newInstance().newTopicMapSystem();
+		TopicMap tm = _tms.createTopicMap(baseIRI);
+		
+		XTM20TopicMapReader reader = new XTM20TopicMapReader(tm, getClass().getResourceAsStream("toytm.xtm"), baseIRI);
+		
+		reader.read();
+		
+		_sail = new TmapiStore(_tms, CONFIG.LIVE);
+		_tmapiRepository = new SailRepository(_sail);
+		_tmapiRepository.initialize();
+		_con = _tmapiRepository.getConnection();
+	}
+
+	
+	
+	
+	/**
+	 * Test.
+	 * 
+	 */
+	@Test
+	public void testConstruct() throws Exception {
+		_sail = new TmapiStore(_tms, CONFIG.LIVE);
+		_tmapiRepository = new SailRepository(_sail);
+		_tmapiRepository.initialize();
+		_con = _tmapiRepository.getConnection();
+		RDFHandler rdfWriter = new N3Writer(System.out);
+		_con .exportStatements(_con .getValueFactory().createURI("http://dbpedia.org/resource/France"), null, null, true, rdfWriter );
+
+
+	
+
+	}
+	
+
+
+
+
+	
+	
+
+}
