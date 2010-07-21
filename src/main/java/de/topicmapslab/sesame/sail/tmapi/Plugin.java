c7e73661578ab193dc5e180caf1bd2f04fee91da
arnim.bleier <arnim.bleier@localhost>
2010-07-21 13:50:06 +0200

    set up plugin

---
+++ src/main/java/de/topicmapslab/sesame/sail/tmapi/Plugin.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright: Copyright 2010 Topic Maps Lab, University of Leipzig. http://www.topicmapslab.de/
+ * License:   Apache License, Version 2.0 http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+package de.topicmapslab.sesame.sail.tmapi;
+
+import org.tmapi.core.Locator;
+import org.tmapi.core.TopicMap;
+
+import de.topicmapslab.sesame.sail.tmapi.utils.TmapiStatementIterator;
+
+/**
+ * @author Arnim Bleier
+ *
+ */
+public interface Plugin {
+	
+	void evaluate(Locator subject, Locator predicate, Locator object, TopicMap tm, TmapiStatementIterator<?> statementIterator );
+	
+}
