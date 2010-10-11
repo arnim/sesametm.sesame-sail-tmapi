2cd264c0dcfa814393e8c99fc0a84a1d0b1cb9b3
arnim.bleier <arnim.bleier@localhost>
2010-10-11 13:18:25 +0200

    version-0.1.0

--- src/test/java/de/topicmapslab/sesame/sail/tmapi/MaianaTest.java
+++ src/test/java/de/topicmapslab/sesame/sail/tmapi/MaianaTest.java
@@ -5,6 +5,9 @@
 
 package de.topicmapslab.sesame.sail.tmapi;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 import junit.framework.TestCase;
 
 import org.junit.Before;
@@ -41,12 +44,19 @@ public class MaianaTest extends TestCase {
 	public void setUp() throws Exception {
 		_tms = TopicMapSystemFactory.newInstance().newTopicMapSystem();
 		TopicMap tm = _tms.createTopicMap(baseIRI);
-		Topic deutschland = tm.createTopicBySubjectIdentifier(tm.createLocator(wiki
-				+ "Deutschland"));
+		Topic deutschland = tm.createTopicBySubjectIdentifier(tm.createLocator("http://dbpedia.org/resource/Germany"));
+		deutschland.addSubjectIdentifier(tm.createLocator("http://en.wikipedia.org/wiki/Germany"));
 		Topic population = tm.createTopicBySubjectIdentifier(tm.createLocator(wiki
 				+ "Population"));
+
+		Collection<Topic> scope = new HashSet<Topic>();
+		scope.add(tm.createTopicBySubjectIdentifier(tm.createLocator(wiki
+				+ "Estimation")));
+		scope.add(tm.createTopicBySubjectIdentifier(tm.createLocator(wiki
+				+ "2008")));
+		
 		deutschland.createOccurrence(population, "82060000", tm
-				.createLocator("http://www.w3.org/2001/XMLSchema#long"));
+				.createLocator("http://www.w3.org/2001/XMLSchema#long"), scope);
 		
 		
 		_sail = new TmapiStore(_tms);
@@ -67,17 +77,16 @@ public class MaianaTest extends TestCase {
 		_tmapiRepository.initialize();
 		_con = _tmapiRepository.getConnection();
 
-
 		String queryString = "PREFIX wiki: <http://en.wikipedia.org/wiki/>  " +
-		" CONSTRUCT { <http://en.wikipedia.org/wiki/Deutschland> ?P ?O }" +
-		"WHERE { <http://en.wikipedia.org/wiki/Deutschland> ?P ?O }";		
+		" CONSTRUCT { <http://en.wikipedia.org/wiki/Germany> ?P ?O }" +
+		"WHERE { <http://en.wikipedia.org/wiki/Germany> ?P ?O }";		
 				
 		GraphQuery query = _con.prepareGraphQuery(QueryLanguage.SPARQL,
 				queryString);
 		GraphQueryResult result = query.evaluate();
 		assertTrue(result.hasNext());
 		Statement ne = result.next();
-		assertEquals("http://en.wikipedia.org/wiki/Deutschland", ne.getSubject().stringValue());
+		assertEquals("http://en.wikipedia.org/wiki/Germany", ne.getSubject().stringValue());
 		assertEquals("http://en.wikipedia.org/wiki/Population", ne.getPredicate().stringValue());
 		assertEquals("82060000", ne.getObject().stringValue());
 		assertEquals("\"82060000\"^^<http://www.w3.org/2001/XMLSchema#long>", ne.getObject().toString());
