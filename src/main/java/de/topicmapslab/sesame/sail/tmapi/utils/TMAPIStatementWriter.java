18c3b30c53ca6b00a3415ef40794c9daa6984eaf
arnim.bleier <arnim.bleier@localhost>
2010-10-11 18:04:53 +0200

    still experimenting with CRUD

---
+++ src/main/java/de/topicmapslab/sesame/sail/tmapi/utils/TMAPIStatementWriter.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright: Copyright 2010 Topic Maps Lab, University of Leipzig. http://www.topicmapslab.de/
+ * License:   Apache License, Version 2.0 http://www.apache.org/licenses/LICENSE-2.0.html
+ */
+
+package de.topicmapslab.sesame.sail.tmapi.utils;
+
+import org.openrdf.model.Resource;
+import org.openrdf.model.URI;
+import org.openrdf.model.Value;
+import org.openrdf.model.impl.LiteralImpl;
+import org.tmapi.core.Locator;
+import org.tmapi.core.Topic;
+import org.tmapi.core.TopicMap;
+
+/**
+ * @author Arnim Bleier
+ * 
+ */
+public class TMAPIStatementWriter {
+	
+	private TopicMap tm;
+	private Topic subject;
+	private Topic predicate;
+	private Value object;
+
+	public TMAPIStatementWriter(Resource subject, URI predicate, Value object, TopicMap tm){
+		this.tm = tm;
+		this.subject = createTopic(tm.createLocator(subject.stringValue()));
+		this.predicate = createTopic(tm.createLocator(predicate.stringValue()));
+		this.object = object;
+	}
+	
+	public void write(){
+		createAsCharacteristic();
+	}
+	
+	
+	private void createAsCharacteristic(){
+		try {
+			URI dt = ((LiteralImpl) object).getDatatype();
+			if (dt != null)
+				subject.createOccurrence(predicate, object.stringValue(), tm.createLocator(dt.stringValue()));
+			else // default xsd:string as dataType
+				subject.createOccurrence(predicate, object.stringValue(), tm.createLocator("http://www.w3.org/2001/XMLSchema#string"));
+		} catch (ClassCastException e) {
+			// Has an URI as Object.
+			subject.createOccurrence(predicate, object.stringValue(), tm.createLocator("http://www.w3.org/2001/XMLSchema#anyURI"));
+		}
+	}
+	
+	
+	
+	public Topic createTopic(Locator l) {
+		Topic t = null;
+		t = tm.getTopicBySubjectIdentifier(l);
+		if (t != null)
+			return t;
+		t = tm.getTopicBySubjectLocator(l);
+		if (t != null)
+			return t;
+		try {
+			t = (Topic) tm.getConstructByItemIdentifier(l);
+		} catch (ClassCastException e) {
+			// Found Construct is not a Topic
+		}
+		return tm.createTopicByItemIdentifier(l);
+	}
+
+}
