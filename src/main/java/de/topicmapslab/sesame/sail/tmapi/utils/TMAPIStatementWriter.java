9271d683577f292f3347866b68d28fcb610e4c65
arnim.bleier <arnim.bleier@localhost>
2010-10-12 14:26:38 +0200

    some further steps

--- src/main/java/de/topicmapslab/sesame/sail/tmapi/utils/TMAPIStatementWriter.java
+++ src/main/java/de/topicmapslab/sesame/sail/tmapi/utils/TMAPIStatementWriter.java
@@ -5,14 +5,20 @@
 
 package de.topicmapslab.sesame.sail.tmapi.utils;
 
+import java.util.Collection;
+import java.util.Iterator;
+
 import org.openrdf.model.Resource;
 import org.openrdf.model.URI;
 import org.openrdf.model.Value;
 import org.openrdf.model.impl.LiteralImpl;
 import org.openrdf.model.vocabulary.XMLSchema;
+import org.tmapi.core.Association;
 import org.tmapi.core.Locator;
+import org.tmapi.core.Occurrence;
 import org.tmapi.core.Topic;
 import org.tmapi.core.TopicMap;
+import org.tmapi.index.LiteralIndex;
 
 /**
  * @author Arnim Bleier
@@ -24,22 +30,54 @@ public class TMAPIStatementWriter {
 	private Topic subject;
 	private Topic predicate;
 	private Value object;
+	private LiteralIndex index;
+	private boolean subjectIsNew = false;
+	private String subjectValue;
+	public final String subjectRoleTypeString = "http://www.tmapi.org/2.0/api#SubjectRoleType";
+	private Topic subjectRoleType;
+	
 
 	public TMAPIStatementWriter(Resource subject, URI predicate, Value object, TopicMap tm){
 		this.tm = tm;
 		this.subject = createTopic(tm.createLocator(subject.stringValue()));
+		this.subjectValue = subject.stringValue();
 		this.predicate = createTopic(tm.createLocator(predicate.stringValue()));
 		this.object = object;
+		index = tm.getIndex(LiteralIndex.class);
+		if (!index.isOpen())
+			index.open();
+		else if (!index.isAutoUpdated())
+			index.reindex();
 	}
 	
-	public void write(){
-		createAsCharacteristic();
-		createAsAssociation();
-	}
-	
-	
-	private void createAsAssociation() {
+	public void write(){	
 		
+		if (subjectIsNew){
+			Collection<Occurrence> occurences = index.getOccurrences(tm.createLocator(subjectValue));
+			if (!occurences.isEmpty()) {
+				associateOccurences(occurences);
+			}
+		}
+		
+		
+		
+
+		createAsCharacteristic();
+	}
+	
+	
+	private void associateOccurences(Collection<Occurrence> occurences) {
+		Iterator<Occurrence> it = occurences.iterator();
+		Occurrence o;
+		while (it.hasNext()) {
+			o =  it.next();
+			this.subjectRoleType =  createTopic(tm.createLocator("http://www.tmapi.org/2.0/api#SubjectRoleType"));
+			Association a = tm.createAssociation(o.getType());
+			a.createRole(subjectRoleType, o.getParent());
+			a.createRole(o.getType(), createTopic(tm.createLocator(o.getValue())));
+			o.remove();
+			
+		}
 	}
 
 	private void createAsCharacteristic(){
@@ -70,6 +108,7 @@ public class TMAPIStatementWriter {
 		} catch (ClassCastException e) {
 			// Found Construct is not a Topic
 		}
+		this.subjectIsNew  = true;
 		return tm.createTopicByItemIdentifier(l);
 	}
 
--- src/test/java/de/topicmapslab/sesame/sail/tmapi/CRUDTest.java
+++ src/test/java/de/topicmapslab/sesame/sail/tmapi/CRUDTest.java
@@ -19,6 +19,7 @@ import org.openrdf.repository.RepositoryResult;
 import org.openrdf.repository.sail.SailRepository;
 import org.openrdf.rio.RDFFormat;
 import org.openrdf.rio.n3.N3Writer;
+import org.tmapi.core.TopicMap;
 import org.tmapi.core.TopicMapSystem;
 import org.tmapi.core.TopicMapSystemFactory;
 
@@ -66,31 +67,39 @@ public class CRUDTest extends TestCase {
 //		_con.export( new N3Writer(System.out), valueFactory.createURI(baseURI));
 //
 //	}
-	
-	@Test
-	public void test2Occurences() throws Exception {
-		is = accessor.convertStringToInputStream("2Occurences.n3");
-		
-		_con.add(is, baseIRI, RDFFormat.N3, valueFactory.createURI(baseURI));
-		assertEquals(5, _con.getStatements(null, null, null, true).asList().size());
-		assertEquals(5, _con.getStatements(null, null, null, true, valueFactory.createURI(baseURI)).asList().size());
-		assertEquals(1, _con.getContextIDs().asList().size());
-		repositoryResult = _con.getStatements(null, null, null, true);
-		
-		_con.export( new N3Writer(System.out), valueFactory.createURI(baseURI));
-
-	}
+//	
+//	@Test
+//	public void test2Occurences() throws Exception {
+//		is = accessor.convertStringToInputStream("2Occurences.n3");
+//		
+//		_con.add(is, baseIRI, RDFFormat.N3, valueFactory.createURI(baseURI));
+//		assertEquals(5, _con.getStatements(null, null, null, true).asList().size());
+//		assertEquals(5, _con.getStatements(null, null, null, true, valueFactory.createURI(baseURI)).asList().size());
+//		assertEquals(1, _con.getContextIDs().asList().size());
+//		repositoryResult = _con.getStatements(null, null, null, true);
+//		
+//		_con.export( new N3Writer(System.out), valueFactory.createURI(baseURI));
+//
+//	}
 	
 	
 	@Test
 	public void test2Association() throws Exception {
 		is = accessor.convertStringToInputStream("2Association.n3");
 		_con.add(is, baseIRI, RDFFormat.N3, valueFactory.createURI(baseURI));
+		TopicMap tm = _tms.getTopicMap(baseURI);
+		assertEquals(1, tm.getAssociations().size());
+		
+		System.out.println(tm.getTopics());
 		
 		
-		assertEquals(2, _con.getStatements(null, null, null, true).asList().size());
-		assertEquals(2, _con.getStatements(null, null, null, true, valueFactory.createURI(baseURI)).asList().size());
+		assertEquals(4, tm.getTopics().size());
+
 		
+//		
+//		assertEquals(2, _con.getStatements(null, null, null, true).asList().size());
+//		assertEquals(2, _con.getStatements(null, null, null, true, valueFactory.createURI(baseURI)).asList().size());
+//		
 		assertEquals(1, _con.getContextIDs().asList().size());
 		repositoryResult = _con.getStatements(null, null, null, true);
 		
